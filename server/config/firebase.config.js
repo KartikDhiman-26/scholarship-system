d0dc865d3f369f1366f0e757ee73fa5cca13e16f
KartikDhiman-26 <kartikdhiman80@gmail.com>
2026-02-24 00:55:59 +0530

    Fixed Firebase production config

--- server/config/firebase.config.js
+++ server/config/firebase.config.js
@@ -1,9 +1,9 @@
 import admin from "firebase-admin";
 
+const firebaseKey = JSON.parse(process.env.FIREBASE_KEY);
+
 admin.initializeApp({
-  credential: admin.credential.cert(
-    JSON.parse(process.env.FIREBASE_KEY)
-  ),
+  credential: admin.credential.cert(firebaseKey),
 });
 
 export const db = admin.firestore();
